492c49c874d572a5cd77ab56939380169b2df7aa
fernandes-dev <eduardo.yugan@gmail.com>
2021-05-01 02:26:45 -0400

    feat: new auth verification

--- src/middlewares/auth_middleware.ts
+++ src/middlewares/auth_middleware.ts
@@ -1,14 +1,20 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import * as jwt from "jsonwebtoken";
 
-export const verify = async (event: APIGatewayProxyEvent) => {
+interface Token {
+  email: string;
+  iat: number;
+  exp: number;
+}
+
+const verify = async (event: APIGatewayProxyEvent, secret = process.env.SECRET): Promise<Token> => {
   const authHeader = event.headers.Authorization;
   const token = authHeader && authHeader.split(" ")[1];
 
   if (token == null) throw new Error("No provided token");
 
   return new Promise((resolve, reject) => {
-    jwt.verify(token, process.env.SECRET as string, (err: any, user: any) => {
+    jwt.verify(token, secret as string, (err: any, tokenData: Token) => {
       if (err)
         reject({
           statusCode: 401,
@@ -16,7 +22,36 @@ export const verify = async (event: APIGatewayProxyEvent) => {
             message: "Invalid jwt token ",
           }),
         });
-      else resolve(user);
+      else resolve(tokenData);
     });
   });
 };
+
+const verifyAll = async (event: APIGatewayProxyEvent): Promise<Token> => {
+  const authHeader = event.headers.Authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) throw new Error("No provided token");
+
+  return new Promise((resolve, reject) => {
+    // primeiro verifica token de aluno
+    jwt.verify(token, process.env.SECRET as string, (err: any, tokenData: Token) => {
+      if (err) {
+        // caso dê erro no token do aluno, verifica se é do personal
+        jwt.verify(token, process.env.SECRET_PERSONAL as string, (err2: any, tokenData2: Token) => {
+          if (err2)
+            reject({
+              statusCode: 401,
+              body: JSON.stringify({
+                message: "Invalid jwt token ",
+              }),
+            });
+          else resolve(tokenData2);
+        })
+      }
+      else resolve(tokenData);
+    });
+  });
+};
+
+export { verify, verifyAll }
